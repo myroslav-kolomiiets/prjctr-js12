7930dce9b5c2796a467a10bab9e8303c0aa7e8ab
myroslav-kolomiiets <mir.kolomiets@gmail.com>
2024-06-07 19:38:02 +0300

    OOP

---
+++ 14_OOP/map.js
@@ -0,0 +1,21 @@
+/**
+ * Custom map
+ * @param fn
+ * @return {*[]}
+ */
+
+// Array.prototype.customMap = function(fn) {
+//     const res = [];
+//     for (let i = 0; i < this.length; i++) {
+//         res.push(fn(this[i]));
+//     }
+//     return res;
+// }
+//
+// const mult = item => item * 2;
+//
+// console.log([1,2,3,4,5,6,7,8,9,10].customMap(mult));
+
+
+
+console.log(null || 0)
